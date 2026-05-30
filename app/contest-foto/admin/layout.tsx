1795790c4abb29f267abd7967c86287678b0779c
Riccardo Lizi <riccardolizzi95@gmail.com>
2026-05-30 07:10:30 +0200

    fix: admin contest 2

--- app/contest-foto/admin/layout.tsx
+++ app/contest-foto/admin/layout.tsx
@@ -1,3 +1,7 @@
-export default function ContestFotoAdminLayout({ children }: { children: React.ReactNode }) {
+export default function ContestFotoLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return <>{children}</>
 }
